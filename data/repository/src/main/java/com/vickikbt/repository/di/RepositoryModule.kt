055fe4910145476a2cb806eca1b615b46b67e991
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2021-10-19 20:05:14 +0300

    feat: domain and repository DI modules setup

--- data/repository/src/main/java/com/vickikbt/repository/di/RepositoryModule.kt
+++ data/repository/src/main/java/com/vickikbt/repository/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.vickikbt.repository.di
 
+import com.vickikbt.domain.repositories.MoviesRepository
+import com.vickikbt.repository.data_sources.MoviesRepositoryImpl
 import org.koin.dsl.module
 
-val repositoryModule= module {
-
+val repositoryModule = module {
+    single<MoviesRepository> { MoviesRepositoryImpl(get(), get()) }
 }
--- domain/src/main/java/com/vickikbt/domain/di/DomainModule.kt
+++ domain/src/main/java/com/vickikbt/domain/di/DomainModule.kt
@@ -1,7 +1,12 @@
 package com.vickikbt.domain.di
 
+import com.vickikbt.domain.usecases.GetInTheaterMovies
+import com.vickikbt.domain.usecases.GetPopularMovies
+import com.vickikbt.domain.usecases.GetTop250Movies
 import org.koin.dsl.module
 
-val domainModule=module{
-
+val domainModule = module {
+    single { GetInTheaterMovies(get()) }
+    single { GetPopularMovies(get()) }
+    single { GetTop250Movies(get()) }
 }
