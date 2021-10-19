b74f35e8db0717f67f3579b3e1587b06d50a8a99
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2021-10-19 18:54:26 +0300

    feat: Added movies API endpoints
    The endpoints include in theaters, popular movies and top 250 movies endpoint.

--- commons/src/main/java/com/vickikbt/commons/Constants.kt
+++ commons/src/main/java/com/vickikbt/commons/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
 
     const val BASE_URL="https://imdb-api.com/API/"
 
+    //ToDo: Remove from public accessible files
+    const val API_KEY="k_am3lgy82"
+
 }
--- data/network/src/main/java/com/vickikbt/network/ApiService.kt
+++ data/network/src/main/java/com/vickikbt/network/ApiService.kt
@@ -1,4 +1,22 @@
 package com.vickikbt.network
 
+import com.vickikbt.commons.Constants
+import com.vickikbt.network.models.InTheatersResponseDto
+import com.vickikbt.network.models.PopularMoviesResponseDto
+import com.vickikbt.network.models.Top250MoviesResponseDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
+
+    @GET("InTheaters/{apiKey}")
+    suspend fun fetchInTheaterMovies(@Path("apiKey") apiKey: String = Constants.API_KEY): Response<InTheatersResponseDto>
+
+    @GET("MostPopularMovies/{apiKey}")
+    suspend fun fetchPopularMovies(@Path("apiKey") apiKey: String = Constants.API_KEY): Response<PopularMoviesResponseDto>
+
+    @GET("Top250Movies/{apiKey}")
+    suspend fun fetchTop250Movies(@Path("apiKey") apiKey: String = Constants.API_KEY): Response<Top250MoviesResponseDto>
+
 }
